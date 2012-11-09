6ba7ce5237b69e4f1760f19bb60aa7b00e8cf8cc
tianjing <tian___jing@hotmail.com>
2012-11-09 14:38:31 +0800

    add project files

---
+++ Android/SayWordByPicture.App/Photo/PhotoChooser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace SayWordByPicture.App.Photo
+{
+   public class PhotoChooser
+    {
+        public PhotoChooser()
+        {
+
+        }
+        public String FileName;
+        public Stream PhotoSteam;
+        /// <summary>
+        /// select photo
+        /// </summary>
+        public void SelectPhoto()
+        {
+
+        }
+        public event EventHandler<PhotoEventArgs> Completed;
+
+    }
+}
---
+++ Android/SayWordByPicture.TextToSpeech/TextToSpeech.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SayWordByPicture.Lib.Core;
+using SayWordByPicture.Data;
+
+namespace SayWordByPicture.TextToSpeech
+{
+    public class TextToSpeech
+    {
+        public void GetSoud(Language p_Language,Word p_Word)
+        {
+        
+        }
+    }
+}
